3077db89af50b591eca23e8348996f2c215f92d9
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2021-12-30 20:08:16 +0530

    Update __main__.py

--- ZeusXRobot/__main__.py
+++ ZeusXRobot/__main__.py
@@ -79,7 +79,7 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-──『[GOD OF THUNDER](https://telegra.ph/file/4c33a90f7df190fd81763.jpg)』──
+──『[NOBISUKI](https://telegra.ph/file/f1870a7c0b73bb6a5defc.jpg)』──
 
 *Hello User!!*
 𝐇𝐞𝐲 𝐈'𝐦 [๖ۣۜZΞUS༻](https://en.m.wikipedia.org/wiki/Zeus),👋
